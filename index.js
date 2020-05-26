5abd3071bb1f246559e1eacdd5d2cf72acaf55d2
Shehab Sha'lan <shehab.shalan@hotmail.com>
2020-05-26 08:12:28 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,80 @@
+var drums = document.querySelectorAll(".drum").length
+
+for (let i = 0; i < drums; i++) {
+    document.querySelectorAll("button")[i].addEventListener("click", function handleClick() {
+        var buttonInner = this.innerHTML
+        makeSound(buttonInner)
+        buttonAnimation(buttonInner)
+
+    })
+}
+
+document.addEventListener("keydown", function (event) {
+    makeSound(event.key)
+    buttonAnimation(event.key)
+})
+
+function makeSound(key) {
+    switch (key) {
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3")
+            tom1.play();
+
+            break;
+        case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3")
+            tom2.play();
+            break;
+        case "s":
+            var tom3 = new Audio("sounds/tom-3.mp3")
+            tom3.play();
+            break;
+        case "d":
+            var tom4 = new Audio("sounds/tom-4.mp3")
+            tom4.play();
+            break;
+        case "j":
+            var snare = new Audio("sounds/snare.mp3")
+            snare.play();
+            break;
+        case "k":
+            var crash = new Audio("sounds/crash.mp3")
+            crash.play();
+            break;
+        case "l":
+            var kickBase = new Audio("sounds/kick-bass.mp3")
+            kickBase.play();
+            break;
+
+        default: console.log(key)
+            break;
+    }
+}
+
+
+function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed")
+    setTimeout(function () {
+        activeButton.classList.remove("pressed")
+    }, 100)
+}
+
+// this is what happens when you trigger an event
+/////////////////////////////////////////////////////////////////////
+// function anotherAddEventListner(typeOfEvent, callback) {
+//     var eventThatHappened = {
+//         eventType: "keydown",
+//         key: "p",
+//         duration: 2
+//     }
+//     if (eventThatHappened.eventType === typeOfEvent) {
+//         callback(eventThatHappened)
+//     }
+// }
+
+// anotherAddEventListner("keydoown", function (event) {
+//     console.log(event)
+
+// })
+///////////////////////////////////////////////////////////////////////
